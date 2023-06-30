91707268e149a61f3db71f754b7711a212505438
Nishit330 <xnishit@gmail.com>
2023-06-30 21:30:13 +0530

    Added Functions File Added Functions File

--- to_do_list/app.py
+++ to_do_list/app.py
@@ -1,15 +1,5 @@
-def get_todos(filepath = 'todo.txt'):
-      """Read a text file and return a list of todos"""
-      with open(filepath, 'r') as file_local:
-            todo_local = file_local.readlines()  
-      return todo_local
-
-def write_todo(todo_arg,filepath = 'todo.txt'):
-      """write a text file"""
-      with  open(filepath, 'w') as file:
-                  file.writelines(todo_arg)
+from functions import get_todos, write_todos
       
-
 
 while True:
       prompt = input('Type add or show or edit or complete: ').strip()
